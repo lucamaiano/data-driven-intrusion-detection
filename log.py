46ca5b47ab7a96d57778dbef9a708e26acefb4b4
Luca <lucamaiano@gmail.com>
2018-08-21 17:11:51 +0200

    Logs

--- log.py
+++ log.py
@@ -1,11 +1,14 @@
 # Import the module
 import subprocess
 
+
 # Ask the user for input
-host = input("Enter a host to ping: ")	
+host = input("Enter a node to ping: ")	
 
 # Set up the echo command and direct the output to a pipe
 
 output = subprocess.Popen(['ping', '-c 2', host], stdout=subprocess.PIPE).communicate()
 
-print(output)
+
+with open('ping.log', 'a') as log:
+	log.write(str(output))
